2cdc7d05e1d302510fae5cb0424b55b99bfdeaa4
Sagar Shah <sa64r.shah@gmail.com>
2022-05-19 20:41:56 +0100

    videos can now be stiched together automatically

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-from moviePy.editor import VideoFileClip, concatenate_videoclips
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+import os
+
+
+def formatVideoInputs(fileName):
+    return VideoFileClip('videos/' + fileName)
+
+
+videoFileNames = os.listdir("./videos")
+videos = []
+
+for fileName in videoFileNames:
+    videos.append(VideoFileClip('videos/' + fileName))
+
+final_clip = concatenate_videoclips(videos)
+
+final_clip.write_videofile("final_clip.mp4")
