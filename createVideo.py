e3a1d2ad78088f0c9cb691da5b6e55677a58f7ab
Sagar Shah <sa64r.shah@gmail.com>
2022-05-31 21:07:23 +0100

    cleaning up code to make it more decoupled and have clean functions

---
+++ createVideo.py
@@ -0,0 +1,141 @@
+from secrets import randbelow
+from moviepy.editor import *
+import json
+import datetime
+import string
+# CONSTANTS
+VIDEO_CLIP_BUFFER = 50  # used to pad around the duration words are said
+JOIN_CLIP_BUFFER = 10  # real buffer between sections
+VIDEO_OUTPUT_PATH = 'output/'
+
+
+# HELPER FUNCTIONS
+def generateTextClip(word):
+    textClip = TextClip(word['text'], font='Arial',
+                        fontsize=50, color='white')
+    textClip = textClip.set_pos(('center').set_duration(
+        (word['end'] - word['start'])/1000))
+    return textClip
+
+
+# import videos
+def getStartTime():
+    return datetime.datetime.now()
+
+
+def importVideo(filename):
+    print('Reading Video File')
+    filename = 'final_clip'
+    return VideoFileClip(VIDEO_OUTPUT_PATH + filename + '.MP4')
+
+
+# import transcription JSON
+def importTranscription(filename):
+    print('Reading Transcription')
+    return json.load(open('transcriptions/' + filename + '.json'))
+
+
+# create sections to keep
+def createSectionsToKeep(transcription):
+    print('Obtaining Video Clips')
+    sectionsToKeep = []
+    for i in transcription['words']:
+        sectionsToKeep.append([i['start'], i['end']])
+    return sectionsToKeep
+
+
+# creating text clips
+def createVideoWithTextGraphics(transcription, video):
+    print('Creating Text Clips')
+    NEXT_TEXT_CLIP_START_BUFFER = 5
+    textClips = []
+    nextClipStart = transcription['words'][0]['start'] + \
+        NEXT_TEXT_CLIP_START_BUFFER
+    for i in transcription['words']:
+        textClip = TextClip(i['text'].upper().translate(str.maketrans('', '', string.punctuation)), font='Berlin-Sans-FB-Demi-Bold',
+                            fontsize=400, color='white')
+        textClip = textClip.set_pos('center').set_duration(
+            (i['end'] - i['start'] + 2 * JOIN_CLIP_BUFFER)/1000)
+        textClip = textClip.set_start(nextClipStart/1000)
+        nextClipStart = i['end'] + NEXT_TEXT_CLIP_START_BUFFER
+        if(i['confidence'] >= 0.5):
+            textClips.append(textClip)
+    return CompositeVideoClip([video, *textClips])
+
+
+# refine sections to keep
+def checkIfClipsOverLap(i, sectionsToKeep, video):
+    if(i+1 < len(sectionsToKeep)):
+        if(sectionsToKeep[i][1] >= sectionsToKeep[i+1][0] - VIDEO_CLIP_BUFFER):
+            sectionsToKeep[i][1] = sectionsToKeep[i+1][1] + VIDEO_CLIP_BUFFER if sectionsToKeep[i +
+                                                                                                1][1] + VIDEO_CLIP_BUFFER < video.duration * 1000 else video.duration * 1000
+            sectionsToKeep.pop(i+1)
+            checkIfClipsOverLap(i, sectionsToKeep, video)
+
+
+def refineSections(sectionsToKeep, video):
+    print('Refining Clips')
+    for i in range(len(sectionsToKeep)):
+        if(i < len(sectionsToKeep)):
+            if(sectionsToKeep[i][0] - VIDEO_CLIP_BUFFER < 0):
+                sectionsToKeep[i][0] = 0
+            else:
+                sectionsToKeep[i][0] = sectionsToKeep[i][0] - VIDEO_CLIP_BUFFER
+
+            if(sectionsToKeep[i][1] + VIDEO_CLIP_BUFFER > video.duration * 1000):
+                sectionsToKeep[i][1] = video.duration * 1000
+            else:
+                sectionsToKeep[i][1] = sectionsToKeep[i][1] + VIDEO_CLIP_BUFFER
+            checkIfClipsOverLap(i, sectionsToKeep, video)
+
+    sectionsToKeep[0][1] = sectionsToKeep[0][1] - \
+        VIDEO_CLIP_BUFFER + JOIN_CLIP_BUFFER
+
+    sectionsToKeep[len(sectionsToKeep)-1][0] = sectionsToKeep[len(sectionsToKeep)-1][0] + \
+        VIDEO_CLIP_BUFFER - JOIN_CLIP_BUFFER
+
+    for i in range(1, len(sectionsToKeep)-1):
+        sectionsToKeep[i][0] = sectionsToKeep[i][0] + \
+            VIDEO_CLIP_BUFFER - JOIN_CLIP_BUFFER
+        sectionsToKeep[i][1] = sectionsToKeep[i][1] - \
+            VIDEO_CLIP_BUFFER + JOIN_CLIP_BUFFER
+    print(sectionsToKeep)
+
+    return sectionsToKeep
+
+ # cut out sections from video
+
+
+def createFinalVideo(videoToCut, hasText, sectionsToKeep):
+    subClips = []
+    print('Cutting Clips')
+    for i in sectionsToKeep:
+        subClips.append(videoToCut.subclip(
+            (i[0])/1000, (i[1])/1000))
+
+    # join sections together
+    final_clip = concatenate_videoclips(subClips)
+
+    # save video
+    final_clip.write_videofile(
+        VIDEO_OUTPUT_PATH + "final_clip-" + str(VIDEO_CLIP_BUFFER) + '- has text = ' + str(hasText) + ".mp4")
+
+
+def showTimeTaken(startTime):
+    print('Time Taken to Complete: ' + str(datetime.datetime.now() - startTime))
+
+
+# SEQUENCE OF FUNCTIONS
+def main(filename):
+    startTime = getStartTime()
+    video = importVideo(filename)
+    transcription = importTranscription(filename)
+    sectionsToKeep = createSectionsToKeep(transcription)
+    sectionsToKeep = refineSections(sectionsToKeep, video)
+    videoWithText = createVideoWithTextGraphics(transcription, video)
+    createFinalVideo(video, False, sectionsToKeep)
+    createFinalVideo(videoWithText, True, sectionsToKeep)
+    showTimeTaken(startTime)
+
+
+main('final_clip')
--- main.py
+++ main.py
@@ -1,21 +1,36 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import os
-from clip import Clip
 
 ASSEMBLY_AI_API_KEY = os.environ.get('ASSEMBLY_AI_API_KEY')
+VIDEO_OUTPUT_PATH = 'output/'
+VIDEO_INPUT_PATH = './videos'
 
 # Get all the video files in the videos folder
-videoFileNames = os.listdir("./videos")
-videos = []
-clippedVideos = []
 
-# Format each video file
-for fileName in videoFileNames:
-    videos.append(VideoFileClip('videos/' + fileName))
+
+def uploadVideos(videoFilenames):
+    videos = []
+
+    # Format each video file
+    for filename in videoFilenames:
+        videos.append(VideoFileClip('videos/' + filename))
+    return videos
+
 
 # Concatenate all the videos
-final_clip = concatenate_videoclips(videos)
+def concatenateVideos(videos):
+    return concatenate_videoclips(videos)
 
 
 # Save the final video
-final_clip.write_videofile("final_clip.mp4")
+def saveFinalVideo(final_clip):
+    final_clip.write_videofile(VIDEO_OUTPUT_PATH + "final_clip.mp4")
+
+
+def main():
+    videos = uploadVideos(os.listdir(VIDEO_INPUT_PATH))
+    final_clip = concatenateVideos(videos)
+    saveFinalVideo(final_clip)
+
+
+main()
